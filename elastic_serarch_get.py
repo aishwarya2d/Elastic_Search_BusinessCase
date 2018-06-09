91cf3f7353999a91d45efb052f16196baa795073
Aishwarya Dhobale <37836087+aishwarya2d@users.noreply.github.com>
2018-06-09 23:55:23 +0530

    Delete elastic_serarch_get.py

--- elastic_serarch_get.py
+++
@@ -1,9 +0,0 @@
-import json
-import elasticsearch
-es = elasticsearch.Elasticsearch()  # use default of localhost, port 9200
-
-doc1= es.get(index='doctors', doc_type='doctor', id=1)
-print doc1
-
-doc1= es.get(index='doctors', doc_type='doctor', id=2)
-print doc1
